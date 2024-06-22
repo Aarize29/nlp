6532a61cc95c4e7992e4fe54ff87e8ef4da791c3
Aarize Siddique <97665948+Aarize29@users.noreply.github.com>
2024-06-22 22:58:43 +0530

    Update chatbot.js

--- server/chatbot.js
+++ server/chatbot.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI('AIzaSyB1CrATX1PUtQ1EGV0xDN5gUkOSMSDWpJs');
+const genAI = new GoogleGenerativeAI('API_KEY');
 
 async function run(inputText) {
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
